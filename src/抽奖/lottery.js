93da8851db438bd906891468c13ec7d8c5ea3885
Arvin <13022126370@163.com>
2019-02-15 17:52:32 +0800

    update

---
+++ src/抽奖/lottery.js
@@ -0,0 +1,23 @@
+$(function() {
+
+	var rad = 0,
+	oldRad = 0,
+	index = 0;
+
+	$(".lotteryPointer")[0].addEventListener("webkitAnimationEnd", function() {
+		oldRad = rad % 360;
+		$(".lotteryPointer").removeClass("animation")
+		$(".lotteryPointer").css({
+			"transform": "rotate(" + oldRad + "deg)"
+		});
+	})
+	$(".pageBox").on("click", ".lotteryPointer", function() {
+		$(".lotteryPointer").addClass("animation");
+		index = 0;
+		console.log(oldRad, index)
+		rad = index * 45 + 360 * 10 + 22.5;
+		console.log("raddddddddddddd",rad)
+		$("style").append("@keyframes myRotate{from {transform: rotate(" + oldRad + "deg);}to {transform: rotate(" + rad + "deg);}}@-webkit-keyframes myRotate{from {transform: rotate(" + oldRad + "deg);}to {transform: rotate(" + rad + "deg);}}");
+	})
+
+});
